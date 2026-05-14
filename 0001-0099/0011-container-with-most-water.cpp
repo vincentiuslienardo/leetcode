8aaa53c3fcd2247fe9d65b6258a25ae044aa5dfb
Vincentius Lienardo <vincentiuslienardo@gmail.com>
2026-05-14 13:54:23 +0700

    feat: add container with most water solution

---
+++ 0001-0099/0011-container-with-most-water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int best = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int h = min(height[left], height[right]);
+            int area = width * h;
+
+            best = max(best, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return best;
+    }
+};
